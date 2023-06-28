d53ab98371f25ea0b1f25ecc1dc4373f406f28a2
juanchycc <jcaserio@fi.uba.ar>
2023-06-28 16:58:47 -0300

    web funcionando y mas docu

---
+++ fiufit_docs/lib/docs_card_web.dart
@@ -0,0 +1,71 @@
+final List<Map<String, dynamic>> docsCardWeb = [
+  {
+    'title': 'Login de administradores',
+    'images':
+        'https://drive.google.com/uc?export=view&id=1wCoij1lBXsm800dqD4NZASnQvOravbSP https://drive.google.com/uc?export=view&id=1c4iz0c5Z35rP3nFhfqSGr9Jz1qVh2y_q https://drive.google.com/uc?export=view&id=13ECxRSVCUGbqBaJfAPLTsqzrcDGB9X34',
+    'descr':
+        'Para poder ingresar al sistema debe contar con un usuario de administrador registrado. Complete los datos correspondientes en el formulario y haga click en el boton Entrar!'
+  },
+  {
+    'title': 'Registro de administradores',
+    'images':
+        'https://drive.google.com/uc?export=view&id=1MBe7k1X6tyOyBvUhgoH1XxxyUD17n1Ex https://drive.google.com/uc?export=view&id=1JC9I6z4JBNN-pCsu9sqUDWbzGnBLb2O7 https://drive.google.com/uc?export=view&id=1QHX9WUw_RL12ZWSvy03VSZB7mKW-5IR0',
+    'descr':
+        'Si usted es un administrador podrá crear otro usuario de administrador, diríjase al menu de Agregar un Adminisitrador, luego complete el formulario con los datos correspondientes y finalmente clickee en Agregar.'
+  },
+  {
+    'title': 'Listar usuarios del sistema',
+    'images':
+        'https://drive.google.com/uc?export=view&id=1qXnm0OfSRMpm5bx6aCQlONYH7c8twquR https://drive.google.com/uc?export=view&id=1OzOhYTdIRFp55PzoCFDq52bUJJslh00Z',
+    'descr':
+        'Para visualizar la lista de usuarios registrados, diríjase al menu de Usuarios, allí encontrará la lista de los mismos.'
+  },
+  {
+    'title': 'Visualizar perfil de usuario',
+    'images':
+        'https://drive.google.com/uc?export=view&id=13bfmZg0CF036o-I6f7m3S_O0nXg95Sbt https://drive.google.com/uc?export=view&id=1Y_Z2Ickrcownn7deMmvJ2oaoXIlRhH_S',
+    'descr':
+        'Si desea visualizar los detalles de un perfil, deberá ingresar a la lista de los mismos y luego clickear en el icono para acceder al detalle.'
+  },
+  {
+    'title': 'Listado de planes de entrenamiento',
+    'images':
+        'https://drive.google.com/drive/u/2/folders/1z0P9j1a7oo8ZvxrzP6QS8mCNyw5Mx3KP',
+    'descr':
+        'Para visualizar la lista de entrenamientos creados, diríjase al menu de Entrenamientos, allí encontrará la lista de los mismos.'
+  },
+  {
+    'title': 'Visualizacion de plan de entrenamiento',
+    'images':
+        'https://drive.google.com/drive/u/2/folders/1z0P9j1a7oo8ZvxrzP6QS8mCNyw5Mx3KP',
+    'descr':
+        'Si desea visualizar los detalles de un entrenamiento, deberá ingresar a la lista de los mismos y luego clickear en el icono para acceder al detalle.'
+  },
+  {
+    'title': 'Metricas de usuarios',
+    'images':
+        'https://drive.google.com/drive/u/2/folders/1z0P9j1a7oo8ZvxrzP6QS8mCNyw5Mx3KP',
+    'descr': 'asdasdasddasdasds'
+  },
+  {
+    'title': 'Bloquear usuario',
+    'images':
+        'https://drive.google.com/uc?export=view&id=1VuHExIhYDUNGPJbFPQA7WyDie7SwWMqw https://drive.google.com/uc?export=view&id=1SEDFk5qSvUY9QDFke6DB24xt-8rQXJTi https://drive.google.com/uc?export=view&id=1P8TNOToJsUeYoZnu68umvbbP-rbOLmew',
+    'descr':
+        'Para bloquear un Usuario (impedir el ingreso en la aplicación), deberá ingresar a la lista de usuarios, seleccionar el/los usuario/s a ser bloqueados y luego clickear en el botón bloquear.'
+  },
+  {
+    'title': 'Bloquear Plan de Entrenamiento',
+    'images':
+        'https://drive.google.com/drive/u/2/folders/1z0P9j1a7oo8ZvxrzP6QS8mCNyw5Mx3KP https://drive.google.com/drive/u/2/folders/1z0P9j1a7oo8ZvxrzP6QS8mCNyw5Mx3KP https://drive.google.com/drive/u/2/folders/1z0P9j1a7oo8ZvxrzP6QS8mCNyw5Mx3KP',
+    'descr':
+        'Para bloquear un Usuario (impedir el ingreso en la aplicación), deberá ingresar a la lista de usuarios, seleccionar el/los usuario/s a ser bloqueados y luego clickear en el botón bloquear.'
+  },
+  {
+    'title': 'Aprobar solicitud de entrenador reconocido',
+    'images':
+        'https://drive.google.com/drive/u/2/folders/1z0P9j1a7oo8ZvxrzP6QS8mCNyw5Mx3KP https://drive.google.com/drive/u/2/folders/1z0P9j1a7oo8ZvxrzP6QS8mCNyw5Mx3KP https://drive.google.com/drive/u/2/folders/1z0P9j1a7oo8ZvxrzP6QS8mCNyw5Mx3KP',
+    'descr':
+        'Para bloquear un Usuario (impedir el ingreso en la aplicación), deberá ingresar a la lista de usuarios, seleccionar el/los usuario/s a ser bloqueados y luego clickear en el botón bloquear.'
+  },
+];
--- fiufit_docs/lib/screens/home_page.dart
+++ fiufit_docs/lib/screens/home_page.dart
@@ -1,4 +1,6 @@
 import 'dart:io';
+import 'package:fiufit_docs/docs_card_app.dart';
+import 'package:fiufit_docs/docs_card_web.dart';
 import 'package:flutter/material.dart';
 import '../models/doc_cards.dart';
 
@@ -17,19 +19,13 @@ class _HomePageState extends State<HomePage> {
   List<DocCards> listaDocsApp = [];
   List<DocCards> listaDocsWeb = [];
 
-  final filePathApp = 'docs_card_app.txt';
-  final filePathWeb = 'docs_card_back.txt';
-  Future<List<DocCards>> fromFile(String path) async {
+  List<DocCards> fromFile(List<Map<String, dynamic>> docs) {
     List<DocCards> docCardsList = [];
-    String contents = await rootBundle.loadString(path);
 
-    print("contents: $contents");
-    List<String> lines = contents.split('\n');
-
-    for (int i = 0; i < lines.length; i += 3) {
-      String title = lines[i];
-      String pathImage = lines[i + 1];
-      String description = lines[i + 2];
+    for (var d in docs) {
+      String title = d['title'];
+      String pathImage = d['images'];
+      String description = d['descr'];
 
       DocCards docCard = DocCards(
         title: title,
@@ -44,85 +40,74 @@ class _HomePageState extends State<HomePage> {
     return docCardsList;
   }
 
-  Future<bool> getDocs() async {
-    listaDocsApp = await fromFile(filePathApp);
-    listaDocsWeb = await fromFile(filePathWeb);
+  bool getDocs() {
+    listaDocsApp = fromFile(docsCardApp);
+    listaDocsWeb = fromFile(docsCardWeb);
     return true;
   }
 
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
+    getDocs();
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.cyan,
-        title: const Text(
-          "FiuFit - Guias de Usuario",
-          style: TextStyle(color: Colors.white, fontSize: 28),
+        appBar: AppBar(
+          backgroundColor: Colors.cyan,
+          title: const Text(
+            "FiuFit - Guias de Usuario",
+            style: TextStyle(color: Colors.white, fontSize: 28),
+          ),
         ),
-      ),
-      body: FutureBuilder(
-          future: getDocs(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData && snapshot.data == true) {
-              return SingleChildScrollView(
-                physics: const BouncingScrollPhysics(),
-                child: Center(
-                  child: SizedBox(
-                    width: size.width * 0.5,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        const Text(
-                          "Guia de usuario de la aplicación",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 24),
-                        ),
-                        ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: listaDocsApp.length,
-                            itemBuilder: (context, index) {
-                              return InkWell(
-                                  child: Text(
-                                    listaDocsApp[index].title,
-                                    style: const TextStyle(
-                                        fontSize: 22, color: Colors.blue),
-                                  ),
-                                  onTap: () => Navigator.pushNamed(
-                                      context, "doc",
-                                      arguments: [listaDocsApp[index]]));
-                            }),
-                        const SizedBox(
-                          height: 30,
-                        ),
-                        const Text("Guia de usuario del backoffice",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 24)),
-                        ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: listaDocsWeb.length,
-                            itemBuilder: (context, index) {
-                              return InkWell(
-                                  child: Text(
-                                    listaDocsWeb[index].title,
-                                    style: const TextStyle(
-                                        fontSize: 22, color: Colors.blue),
-                                  ),
-                                  onTap: () => Navigator.pushNamed(
-                                      context, "doc",
-                                      arguments: [listaDocsWeb[index]]));
-                            }),
-                      ],
-                    ),
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Center(
+            child: SizedBox(
+              width: size.width * 0.5,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Guia de usuario de la aplicación",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24),
                   ),
-                ),
-              );
-            } else {
-              return const Center(child: CircularProgressIndicator());
-            }
-          }),
-    );
+                  ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: listaDocsApp.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                            child: Text(
+                              listaDocsApp[index].title,
+                              style: const TextStyle(
+                                  fontSize: 22, color: Colors.blue),
+                            ),
+                            onTap: () => Navigator.pushNamed(context, "doc",
+                                arguments: [listaDocsApp[index]]));
+                      }),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  const Text("Guia de usuario del backoffice",
+                      style:
+                          TextStyle(fontWeight: FontWeight.bold, fontSize: 24)),
+                  ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: listaDocsWeb.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                            child: Text(
+                              listaDocsWeb[index].title,
+                              style: const TextStyle(
+                                  fontSize: 22, color: Colors.blue),
+                            ),
+                            onTap: () => Navigator.pushNamed(context, "doc",
+                                arguments: [listaDocsWeb[index]]));
+                      }),
+                ],
+              ),
+            ),
+          ),
+        ));
   }
 }
 
